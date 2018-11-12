c9d69c423493044064d837b18294b9043be89bd4
Yanis <44744200+yanistig@users.noreply.github.com>
2018-11-12 14:58:01 +0100

    fghj

--- MiniSGBD/Code/PageId.java
+++ MiniSGBD/Code/PageId.java
@@ -1,30 +1,49 @@
 
 public class PageId {
-	
-	private int FileIdx;
+
+	private int FileIdx ; 
 	private int PageIdx;
+	
+	
+	
+
 	/**
-	 * 
-	 * @return
+	 * Constructeur de la classe PageId
+	 * @param fileIdx
+	 * @param pageIdx
 	 */
-	
-	
-	public int getFileIdx() {
-		return FileIdx;
-	}
-	public void setFileIdx(int fileIdx) {
-		FileIdx = fileIdx;
-	}
-	public int getPageIdx() {
-		return PageIdx;
-	}
-	public void setPageIdx(int pageIdx) {
-		PageIdx = pageIdx;
-	}
 	public PageId(int fileIdx, int pageIdx) {
 		super();
 		FileIdx = fileIdx;
 		PageIdx = pageIdx;
 	}
 
+	/**
+	 * 
+	 * @return FileIndx l'identifiant du fichier ou est contenu la page 
+	 */
+	public int getFileIdx() {
+		return FileIdx;
+	}
+
+	/**
+	 * 
+	 * @param fileIdx
+	 */
+	public void setFileIdx(int fileIdx) {
+		FileIdx = fileIdx;
+	}
+
+	/**
+	 * 
+	 * @return PageIdx l'identifiant de la page
+	 */
+	public int getPageIdx() {
+		return PageIdx;
+	}
+
+	
+	public void setPageIdx(int pageIdx) {
+		PageIdx = pageIdx;
+	}
 }
