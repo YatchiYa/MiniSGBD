d1ae568638ed092301d213ab50636fed29a184e2
YatchiYa <35928424+YatchiYa@users.noreply.github.com>
2018-11-12 13:21:14 +0100

    de

--- MiniSGBD/Code/BufferTable.java
+++ MiniSGBD/Code/BufferTable.java
@@ -1,17 +1,33 @@
+import java.util.Arrays;
 
 public class BufferTable {
 
 	private static final long sizePage = 4096;
-
+	
+	private InfoPage inf;
 	private byte[] buffer;
 
 	
+	public BufferTable(InfoPage inf, byte[] buffer) {
+		super();
+		this.inf = inf;
+		this.buffer = buffer;
+	}
+
 	
 	
-	
-	
-	
-	
+	public InfoPage getInf() {
+		return inf;
+	}
+
+	public void setInf(InfoPage inf) {
+		this.inf = inf;
+	}
+
+	public static long getSizepage() {
+		return sizePage;
+	}
+
 	/**
 	 * 
 	 * @return
@@ -26,6 +42,13 @@ public class BufferTable {
 	 */
 	public void setBuffer(byte[] buffer) {
 		this.buffer = buffer;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "BufferTable [inf=" + inf + ", buffer=" + Arrays.toString(buffer) + "]";
 	}
 	
 	
