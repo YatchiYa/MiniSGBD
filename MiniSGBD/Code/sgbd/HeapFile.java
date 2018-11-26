0a7cd872d80266c9da65ee2fc6bbe80b7e21159e
Yanis <44744200+yanistig@users.noreply.github.com>
2018-11-26 11:19:56 +0100

    YanZimZim
    tu dors Joseph ?

--- MiniSGBD/Code/sgbd/HeapFile.java
+++ MiniSGBD/Code/sgbd/HeapFile.java
@@ -26,21 +26,38 @@ public class HeapFile {
 	 */
 	public void createNewOnDisk () throws IOException {
 		DiskManager.CreateFile(relation.getFileIdx());	
-	
+		//on crée une instance de la Header Page
+		PageId hp= new PageId(relation.getFileIdx(),0);
 		// ajout de la HeaderPage je suis pas sur !!!!
-		DiskManager.AddPage(relation.getFileIdx(), 0);
+		DiskManager.AddPage(relation.getFileIdx(), 0);    // revenir revoir plus tard 
+		
+		
+		byte[] bufferHeaderPage = BufferManager.getPage(hp);
+		// construire une HeaderPageInfo toute fraîche (avec donc le dataPageCount=0)
+		HeaderPageInfo Hpi = new HeaderPageInfo(0);
+		//ecrire la HeaderPageInfo dans le buffer 
+		Hpi.writeToBuffer(bufferHeaderPage, Hpi);  // revoir 
+		BufferManager.freePageId(hp, 1); // libreer le buffer en signalant u'il a été modifié 
 		
 	}
 	
 	
-	/*public void getFreePageId(oPageId) {
-		
+	public void getFreePageId(PageId oPageId) throws IOException {
+		byte[ ] bufferHeaderPage = BufferManager.getPage(oPageId);
 	}
+	 
+	/**
+	* Cette méthode devra actualiser les informations dans la Header
+	*  Page suite à l’occupation d’une des cases disponible sur une page
 	*/
-	
-	/*public void updateHeaderWithTakenSlot(iPageId) {
-		
+	public void updateHeaderWithTakenSlot(PageId iPageId) throws IOException{
+		//on recupére l'indice du fichier HeapFIle
+		int FileIdxHP = relation.getFileIdx();
+		//on crée une instance de headerPage
+		PageId hp=  new PageId(FileIdxHP,0);
+		byte[ ] bufferHeaderPage = BufferManager.getPage(hp);
+		HeaderPageInfo hpi = new HeaderPageInfo(0);
 	}
-	*/
+	
 	
 }
